0279aa18fb79160c2569550533eeae9f782df9b9
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-04 17:34:38 +0100

    added rake database tasks

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,4 +6,3 @@ require './app/models/link'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
