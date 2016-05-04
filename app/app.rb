b7e606307080b1836e909d7a1206cddde8e93bb5
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-04 10:35:33 +0100

    configured development environment

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,10 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require_relative './models/link'
 
 class Bookmark_manager < Sinatra::Base
+
   get '/' do
     redirect '/links'
   end
